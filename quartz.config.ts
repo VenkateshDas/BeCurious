db83d630ecbb4afebdd85e7f0c1067fb70aaf85f
VenkateshDas <venkateshdas.me@gmail.com>
2026-03-27 00:53:51 +0530

    publish: 2026-03-27 00:53

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Be Curious Garden",
-    pageTitleSuffix: " | Be Curious Garden",
+    pageTitle: "Curiosity Garden",
+    pageTitleSuffix: " | Curiosity Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
